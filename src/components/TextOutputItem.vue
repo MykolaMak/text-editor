7db0db2ed31801a911a8bbc514cafef1a49d4c23
MykolaMak <makolamak@gmail.com>
2020-06-11 01:52:32 +0300

    need implement render "\n" and check for some bags

---
+++ src/components/TextOutputItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <span v-bind:style="styledObject">{{ item.text }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      textArr: this.item.text.split('\n'),
+      styledObject: {
+        color: this.item.color,
+        backgroundColor: this.item.background,
+        fontSize: this.item.fontSize + 'px',
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
